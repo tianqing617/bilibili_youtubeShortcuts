86b9e91c868b30dbbf17867e25d3ad708d9f2220
Liu Tianqing <liutianqing@dfsoft.com.cn>
2020-02-27 18:25:44 +0800

    feat: v1.0

--- bilibili.shortcuts.user.js
+++ bilibili.shortcuts.user.js
@@ -14,6 +14,30 @@
   const shortcutMethodNames = {
     'f': 'fullScreenMode',
     'w': 'webScreenMode',
+    't': 'threatreScreenMode',
+    'm': 'muteMode',
+    'c': 'barrageMode',
+    'r': 'repeatMode',
+  }
+
+  // 根据class名称，获取dom对象
+  function getBtnObjByClass(name) {
+    const btnObj = document.getElementsByClassName(name);
+    if (btnObj && btnObj.length === 1) {
+      return btnObj[0];
+    } else {
+      console.log('网页代码被改变，脚本已不适用！');
+      return null;
+    }
+  }
+
+  // 执行（单击）DOM对象
+  function clickButton(button) {
+    if (button) {
+      button.click();
+    } else {
+      console.log('网页代码被改变，脚本已不适用！');
+    }
   }
 
   // 点击快捷键，触发的方法
@@ -22,25 +46,33 @@
     fullScreenMode: function() {
       console.log('full screen mode');
       // 找到功能键的容器
-      const btnObjects = document.getElementsByClassName('bilibili-player-video-web-fullscreen');
-      if (btnObjects && btnObjects.length === 1) {
-        const myButton = btnObjects[0];
-        if (myButton) myButton.click();
-        // children为每个功能键容器下面的两个button
-        // const children = btnObjects[0].children;
-        // if (children && children.length === 2) {
-        //   children[0].click();
-        // }
-      }
+      clickButton(getBtnObjByClass('bilibili-player-video-web-fullscreen'));
     },
     // 网页全屏
     webScreenMode: function() {
-      console.log('web screen mode');
-    }
+      clickButton(getBtnObjByClass('bilibili-player-video-web-fullscreen'));
+    },
+    // 剧院模式
+    threatreScreenMode: function() {
+      clickButton(getBtnObjByClass('bilibili-player-video-btn-widescreen'));
+    },
+    // 静音
+    muteMode: function() {
+      clickButton(getBtnObjByClass('bilibili-player-iconfont-volume'));
+    },
+    // 字幕
+    barrageMode: function() {
+      // clickButton(getBtnObjByClass('bilibili-player-iconfont-subtitle'));
+      clickButton(getBtnObjByClass('bilibili-player-iconfont-subtitle'));
+    },
+    // 循环
+    repeatMode: function() {
+      clickButton(getBtnObjByClass('bilibili-player-video-btn-repeat'));
+    },
   }
 
   function init() {
-    console.log('init...');
+    // console.log('init...');
     // 注册事件
     document.addEventListener('keydown', function(e) {
       if (e && e.key) {
